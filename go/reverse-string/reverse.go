b6e20d701ea61cb5dce024bd13e382b9561e45f6
Mark Ferlatte <mark@truss.works>
2018-02-08 22:53:34 -0800

    reverse-string

---
+++ go/reverse-string/reverse.go
@@ -0,0 +1,11 @@
+package reverse
+
+// String returns a reversed string
+func String(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	reverse := string(runes)
+	return reverse
+}
